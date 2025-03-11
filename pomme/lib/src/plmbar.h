8dc28639fc7f4b30c77aa5b52ced23d5cc118018
Li-Yin Young <liyo6397@colorado.edu>
2025-03-11 15:03:54 -0600

    The setup.py will install *.so file to src folder by default
    - Remove the Pomme/pomme folder

--- pomme/lib/src/plmbar.h
+++
@@ -1,8 +0,0 @@
-int plmbar_
-(
-   double *p, 
-   double *dp, 
-   double *z, 
-   int    *lmax, 
-   int    *inorm
-);
--- pomme/lib/src/utils.h
+++
@@ -1,6 +0,0 @@
-int i2m(int i);
-
-int imin(int x,int y);
-int imax(int x,int y);
-int indx(int n,int m);
-
