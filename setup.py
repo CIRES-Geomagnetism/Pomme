ae72c73cd901512767309ec36bc65356842de907
Li-Yin Young <liyo6397@colorado.edu>
2025-04-23 11:34:21 -0600

    Make the path and -O2 available to recognize in Windows OS
    - paths to the files are setup by os.path.join
    - If the platform is Windows, the O2 flag become /O2

--- setup.py
+++ setup.py
@@ -1,27 +1,35 @@
+import os
+import platform
 from setuptools import setup, Extension
 from setuptools.command.build_ext import build_ext as _build_ext
+
+pkg_dir = os.path.join("pomme", "cpomme")
+src_dir = os.path.join(pkg_dir, "src")
 
 class build_ext(_build_ext):
     def build_extensions(self):
         super().build_extensions()
 
+compile_args = ["-O2"]
+
+if platform.system() == "Windows":
+    compile_args = ["/O2"]
+
 module = Extension("pomme.cpomme", 
-    sources=["pomme/cpomme/pomme_wrapper.c","pomme/cpomme/model.c",
-    "pomme/cpomme/src/geod2geoc.c",
-    "pomme/cpomme/src/gsm2geo.c",
-    "pomme/cpomme/src/int-ext.c",
-    "pomme/cpomme/src/magfdz.c",
-    "pomme/cpomme/src/plmbar.c",
-    "pomme/cpomme/src/pom.c",
-    "pomme/cpomme/src/sm2geo.c",
-    "pomme/cpomme/src/utils.c"],
-    include_dirs=["pomme/cpomme/src", "pomme/cpomme"],
-    extra_compile_args=["-O2"])
+    sources=[os.path.join(pkg_dir, "model.c"), os.path.join(pkg_dir, "pomme_wrapper.c"),
+    os.path.join(src_dir, "geod2geoc.c"),
+    os.path.join(src_dir, "gsm2geo.c"),
+    os.path.join(src_dir, "int-ext.c"),
+    os.path.join(src_dir, "magfdz.c"),
+    os.path.join(src_dir, "plmbar.c"),
+    os.path.join(src_dir, "pom.c"),
+    os.path.join(src_dir, "sm2geo.c"),
+    os.path.join(src_dir, "utils.c"),
+    ],
+    include_dirs=[src_dir, pkg_dir],
+    extra_compile_args=compile_args)
 
 setup(
-    #name='pomme',
-    #version='0.1.0',
-    #description='Pomme python module',
     ext_modules=[module],
     cmdclass={'build_ext': build_ext},
 )
