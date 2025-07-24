1052edb4ba1094460da6b1210395705860ac0f15
Li-Yin Young <liyo6397@colorado.edu>
2025-07-24 12:02:47 -0600

    Add the instructions to install the wheel and updated the example codes in Readme.md

--- tests/smoke_test_pypomme.py
+++ tests/smoke_test_pypomme.py
@@ -3,7 +3,7 @@ from pomme.pypomme import get_field
 
 decimal_year = 2024.5
 lat = [-90.,-45.,0.,45.,90.]
-lon = [-180.,-90.,0,180.,360.]
+lon = [-180.,-90.,0.,180.,360.]
 alt_km = [-1.,0.,250.,500.,1000.]
 est = 50.5
 ist = 25.5
